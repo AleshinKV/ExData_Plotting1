9890c17a2b9872908e1a0ea58fa0383b4b7fba72
Kirill Aleshin <kirill.aleshin@gmail.com>
2015-06-07 02:04:58 +0500

    Final version to evaluate

--- Plot2.R
+++ Plot2.R
@@ -4,9 +4,10 @@ data <- read.table("household_power_consumption_Query.txt", header = TRUE, sep =
 
 data$datetime <- strptime(paste(strtrim (data$Date,8),data$Time), "%d.%m.%Y %H:%M:%S")
 
-png(file = "plot1.png", bg = "transparent")
+png(file = "plot2.png", bg = "transparent")
 
-hist(data$Global_active_power, xlab = "Global Active Power (kilowatts)", 
-     main = "Global Active Power", col = "red", rlim =c(0,1200))
+plot(data$datetime, data$Global_active_power, type = "l",
+     ylab = "Global Active Power (kilowatts)",
+     xlab="")
 
 dev.off()
